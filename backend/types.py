0f9d27b3ea0061beb4f8a26920b583484e3b9f64
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-11-25 12:43:09 +0100

    Defined types

--- backend/types.py
+++ backend/types.py
@@ -1,7 +1,23 @@
 from pydantic import BaseModel
 
 
-
 class Resume(BaseModel):
     full_name : str
-    
+    phone_number : int
+    location : str
+    socials : dict[str]
+    profile : str 
+    skill : list[str]
+    eductation : dict[str]
+    experience : dict[str] 
+    project : dict[str]
+    hobbies : list[str]
+    languages : list[str]
+
+
+
+class JobDescription(BaseModel):
+    job_poster : str 
+    required_skills : list[str] 
+    tasks : list[str] 
+
