3d968730807d112dc2119e77282cd6100a64b366
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-11-28 14:09:49 +0100

    Implemented openai call

--- backend/model.py
+++ backend/model.py
@@ -1,6 +1,6 @@
 from abc import ABC,abstractmethod
 from langchain_community.llms.ollama import Ollama
-
+from langchain_openai import ChatOpenAI
 
 class Model(ABC):
     
@@ -23,8 +23,9 @@ class OssModel(Model):
 
 class Openai(Model):
 
-    def __init__(self):
-        pass 
+    def __init__(self,model="gpt-4o"):
+        self.model = ChatOpenAI(model)
 
     def _run(self,input) -> str:
-        pass 
+        response = self.model.invoke(input)
+        return response["content"]
